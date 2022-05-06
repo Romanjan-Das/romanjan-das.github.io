fd3c03948222a00f52d6231445e488cb5fc00b9e
Romanjan Das <romanjandas.mail@gmail.com>
2022-05-07 01:01:46 +0530

    added guwahati_local_v2 to repo

---
+++ guwahati_local_v2/index_2.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Refresh" content="2; url=https://test-site-romanjandas.000webhostapp.com/guwahati_local/glv1_modified/index_1.php" />
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $data = fopen("main_ad_ids.txt","r");
+    $string = fread($data,filesize("main_ad_ids.txt"));
+    $pattern = "/\*/";
+    $limit = 2;
+    $for_loop_count = substr_count($string,"*")/2 ;
+
+    for($i=0; $i<$for_loop_count; $i++)
+    {
+        $j = $for_loop_count - $i;
+        $j = $j-1;
+        $string = preg_replace($pattern,$j,$string,$limit);
+    }
+    
+    
+
+    $output_data = fopen("main_ad_ids.js","w");
+    fwrite($output_data,$string);
+    fclose($output_data);
+    
+    fclose($data);
+
+    echo "<h1>Processing completed.</h1>" 
+    ?>
+</body>
+</html>
