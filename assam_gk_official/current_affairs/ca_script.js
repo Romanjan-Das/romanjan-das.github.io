8631de200c21e73e29cd4f365bacfc7620ea79bb
Romanjan Das <romanjandas.mail@gmail.com>
2022-05-20 13:48:00 +0530

    current affairs section implemented satisfactoraly

--- assam_gk_official/current_affairs/ca_script.js
+++ assam_gk_official/current_affairs/ca_script.js
@@ -8,7 +8,8 @@ function next(x) {
     if (x && i<ca_name.length) {
         if(i==ca_name.length-1){
             //do nothing
-            alert("You have reached the end");
+            document.getElementsByClassName("question_section ca_a")[0].style.opacity="0%";
+            document.getElementsByClassName("question_section ca_a")[1].style.display="block";
         }
         else{
             i++;
@@ -17,6 +18,10 @@ function next(x) {
         document.getElementById("ca_content").innerHTML=ca_text[i].innerHTML;
     }
     else if(x==false){
+        if(i==ca_name.length-1){
+            document.getElementsByClassName("question_section ca_a")[0].style.opacity="100%";
+            document.getElementsByClassName("question_section ca_a")[1].style.display="none";
+        }
         if(i>0){
             i=i-1;
             document.getElementById("ca_title").innerHTML=(i+1)+". "+ca_name[i].innerHTML;
