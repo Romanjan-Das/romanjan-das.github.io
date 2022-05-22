aa5fb491b0e30c1b7eb696030a3a5530e5742559
Romanjan Das <romanjandas.mail@gmail.com>
2022-05-22 17:47:19 +0530

    name change and landscape blank

--- assam_gk_official/current_affairs/ca_script.js
+++ assam_gk_official/current_affairs/ca_script.js
@@ -6,7 +6,7 @@ var color_num=0;
 //var color_array=["#CCF3EE","#F9F3EE","#F9CEEE"];
 var color_array=["#FFFFFF","#FFFFFF","#FFFFFF"];
 function next(x) {
-    document.getElementById("timer").innerHTML="অসমীয়া সাধাৰণ জ্ঞান &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+document.getElementById("ca_date").innerText;
+    document.getElementById("timer").innerHTML="Assam General Knowledge &nbsp;&nbsp;&nbsp;"+document.getElementById("ca_date").innerText;
 
     if (x && i<ca_name.length) {
         if(i==ca_name.length-1){
